3dce1f3002ec6c2b02add0f0f15be9754d981b85
Anton Schukin <anton.schukin@corp.badoo.com>
2018-11-08 21:05:49 +0000

    Initial Commit

---
+++ VKContest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKContest
+//
+//  Created by Anton Schukin on 08/11/2018.
+//  Copyright © 2018 Anton Shchukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
