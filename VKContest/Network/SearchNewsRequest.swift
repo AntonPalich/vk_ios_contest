ffc795a3036fc29cb80376562574a21eb5e239f6
Anton Schukin <anton.schukin@corp.badoo.com>
2018-11-11 19:59:44 +0000

    Added foundation for implementing search

--- VKContest/Network/SearchNewsRequest.swift
+++ VKContest/Network/SearchNewsRequest.swift
@@ -1,31 +1,36 @@
 //
-//  GetNewsRequest.swift
+//  SearchNewsRequest.swift
 //  VKContest
 //
-//  Created by Anton Schukin on 10/11/2018.
+//  Created by Anton Schukin on 11/11/2018.
 //  Copyright © 2018 Anton Shchukin. All rights reserved.
 //
 
 import Foundation
 
-class GetNewsRequest: NetworkRequest {
+class SearchNewsRequest: NetworkRequest {
 
+    let query: String
     let startFrom: String?
 
-    init(startFrom: String?) {
+    init(query: String, startFrom: String?) {
+        self.query = query
         self.startFrom = startFrom
     }
 
     private(set) var response: GetNewsResponse?
-    
+
     // MARK: NetworkRequest
 
     var methodName: String {
-        return "newsfeed.get"
+        return "newsfeed.search"
     }
 
     var methodParameters: [String : String] {
-        var parameters = ["filters": "post"]
+        var parameters = [
+            "q": self.query,
+            "extended": "1"
+        ]
         if let startFrom = self.startFrom {
             parameters["start_from"] = startFrom
         }
